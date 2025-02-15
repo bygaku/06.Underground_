2f9b01815a1ce536f871baffde367bebdc4ae591
fca2406100010 <fca2406100010@edu.fca.ac.jp>
2025-02-15 22:23:21 +0900

    -added IScene interface class.

--- Underground_/Underground_/IScene.cpp
+++ Underground_/Underground_/IScene.cpp
@@ -2,3 +2,10 @@
 // @brief  シーンインターフェイス.
 // @remark -2025 Fujii Gaku-
 //-----------------------------------------------------------------------------
+#include <iostream>
+#include "SceneManager.h"
+
+void IScene::ChangeScene(std::string_view sceneName)
+{
+    m_manager.ChangeScene(sceneName);
+}
--- Underground_/Underground_/IScene.h
+++ Underground_/Underground_/IScene.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <string_view>
+
+class SceneManager;
+
+/// @brief シーンインターフェイス
+class IScene
+{
+public:
+	IScene(SceneManager& manager)
+		: m_manager(manager) {};
+
+	virtual ~IScene() = default;
+
+	virtual void Initialize() {};
+	virtual void Update() {};
+	virtual void Draw() const {};
+	virtual void Timer(float deltaTime) {};
+protected:
+	/// @brief シーンを切り替えます。
+	/// @param sceneName 切り替え先のシーン名
+	void ChangeScene(std::string_view sceneName);
+
+	/// @brief シーンの切替前の処理を行います。
+	virtual void SceneFinalizeCondition() {};
+private:
+	IScene& m_manager;	/// @brief シーンマネージャーの参照
+
+protected:
+	float m_timeScene = 0.0f;	/// @brief Time from current scene.
+};
